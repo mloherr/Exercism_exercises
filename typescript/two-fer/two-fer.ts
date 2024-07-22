110e10583288a69e5fc76da6c67255ebcabfa285
mloherr <herraizmaria08@gmail.com>
2024-07-22 17:32:06 +0200

    Added TypeScript exercises

---
+++ typescript/two-fer/two-fer.ts
@@ -0,0 +1,11 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(namePerson?: string): string {
+  if (namePerson) {
+    return `One for ${namePerson}, one for me.`;
+  } else {
+    return "One for you, one for me.";
+  }
+}
